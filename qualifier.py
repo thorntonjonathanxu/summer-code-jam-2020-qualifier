6c575c1022619c714fc410aad048635801bbe797
Jon <thorntonjonathanxu@gmail.com>
2020-07-09 20:04:49 -0400

    Need to implement __repr__

--- qualifier.py
+++ qualifier.py
@@ -28,4 +28,16 @@ class Article:
     """The `Article` class you need to write for the qualifier."""
 
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
-        pass
+      self.title = title
+      self.author = author
+      self.publication_date = publication_date
+      self.content = content
+      pass
+
+    def __repr__(self):
+      print('Article title={0} author={1} publication_date={2}'.format(self.title, self.author, datetime.datetime.isoformat(self.publication_date)))
+      # return {'Article ', 'Title':self.title, 'Author':self.author, 'publication_date'}
+
+
+fairytale = Article(title="The emperor's new clothes",author="Hans Christian Andersen",content="'But he has nothing at all on!' at last cried out all the people. The Emperor was vexed, for he knew that the people were right.",publication_date=datetime.datetime(1837, 4, 7, 12, 15, 0))
+fairytale.__repr__()
